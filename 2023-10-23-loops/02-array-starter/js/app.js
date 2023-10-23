71ed637fa838df55503439d4d013622fed523abe
Tony Grimes <acidtone@tonygrimes.com>
2023-10-23 08:53:11 -0600

    added gallery code

---
+++ 2023-10-23-loops/02-array-starter/js/app.js
@@ -0,0 +1,18 @@
+// A list of pre-selected image IDs from Lorem Picsum
+const imgList = [237, 433, 577, 582, 593, 659, 718, 783, 790, 837, 1024, 1025, 1074, 1084]; 
+
+// Create an empty string variable where we will store the html that will eventually be printed to the page
+
+// Using `document.querySelector()`, assign the .gallery element to a variable.
+  
+
+// Loop through `imgList` using a `for...of` loop. 
+
+
+  // For each image ID, 
+  // 1. create an img element using string literals
+  // 2. point the image `src` to Lorem Picusm, using the current image ID in the URL.
+  // 3. append the `img` html string to the html variable you created earlier
+
+
+// Using element.innHTML, add the html img list to the .gallery variable you created earlier.
